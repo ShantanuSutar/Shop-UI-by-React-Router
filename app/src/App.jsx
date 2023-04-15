1c22f67c88cfa5df41acded4ca89e19326fa52f6
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-15 19:36:38 +0530

    required components setup

---
+++ app/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Error from "./pages/Error.jsx";
+import Posts from "./pages/Posts.jsx";
+import Products from "./pages/Products.jsx";
+import SingleProduct from "./pages/SingleProduct.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<SingleProduct />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
