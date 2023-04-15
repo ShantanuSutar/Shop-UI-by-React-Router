00a452457ab06aca9be33b7728d5b34399c2a01a
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-15 21:32:29 +0530

    styling home component

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>Welcome to WebStylePress</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam iste
+          cum, nesciunt ex dolorem eius eum eveniet tempore! Architecto ipsa,
+          eveniet ad eum sit ex tenetur nostrum omnis laborum non. Rerum sint
+          libero accusamus ratione explicabo. Repellendus expedita atque neque
+          amet quos, quae perferendis earum aperiam quod fugit voluptas omnis
+          sint nobis odio maiores eius tempora modi delectus dolores a.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam iste
+          cum, nesciunt ex dolorem eius eum eveniet tempore! Architecto ipsa,
+          eveniet ad eum sit ex tenetur nostrum omnis laborum non. Rerum sint
+          libero accusamus ratione explicabo. Repellendus expedita atque neque
+          amet quos, quae perferendis earum aperiam quod fugit voluptas omnis
+          sint nobis odio maiores eius tempora modi delectus dolores a.
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
