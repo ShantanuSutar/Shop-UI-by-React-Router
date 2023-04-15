e80e10020347c74008c898eaffcd82c133514d53
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-15 19:41:33 +0530

    basic header and footer setup

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <nav className="d-flex justify-content-center align-items-center">
+          <div className="nav nav-pills ">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/posts">Posts</Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container">
+        <div className="d-flex justify-content-between align-item-center">
+          <div className="logo">
+            <Link to="/" className="brand">
+              WSP
+            </Link>
+          </div>
+          <div className="nav nav-pills ">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/posts">Posts</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
