818553ef728b58adee9ba2299482115392fbcf28
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-21 20:03:50 +0530

    setup to display single product and links to go back to products page

--- app/src/pages/SingleProduct.jsx
+++ app/src/pages/SingleProduct.jsx
@@ -1,7 +1,69 @@
 import React from "react";
-
+import { Link, useNavigate, useParams, useLocation } from "react-router-dom";
+import PRODUCTS from "../data";
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+  const navigate = useNavigate();
+
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-7">
+              <h1>Title</h1>
+            </div>
+
+            <div className="col-lg-5">
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb justify-content-end">
+                  <li className="breadcrumb-item">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="breadcrumb-item">
+                    <Link to="/products">Products</Link>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    Page Title
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container content">
+        <div className="row">
+          <div className="col-lg-5">image</div>
+          <div className="col-lg-7">
+            <h2>name</h2>
+            <p className="price">price</p>
+            <p>details...</p>
+            <br />
+            <button
+              onClick={() => {
+                navigate(-1);
+              }}
+              className="btn btn-primary btn-sm"
+            >
+              BACK
+            </button>
+            <button
+              onClick={() => {
+                navigate("/products");
+              }}
+              className="btn btn-primary btn-sm"
+            >
+              NAVIGATE TO PRODUCTS
+            </button>
+            <Link to="/products" className="btn btn-primary btn-sm">
+              PRODUCTS
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default SingleProduct;
