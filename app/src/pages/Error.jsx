3b92fdeda790b5b45a9c2973930221574d887657
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-21 21:23:36 +0530

    Handling Error Page

--- app/src/pages/Error.jsx
+++ app/src/pages/Error.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>404 Error</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <p>Page not found...</p>
+        <Link to="/" className="btn btn-primary">
+          Go Back to Homepage
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default Error;
